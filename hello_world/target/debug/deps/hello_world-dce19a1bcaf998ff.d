185b649a49fd9fdf9fc1d5e4c30e28df9ed658f9
Daniel Beer <daniel.beer@draper.ai>
2023-12-04 18:18:41 -0500

    delete hello world

--- hello_world/target/debug/deps/hello_world-dce19a1bcaf998ff.d
+++
@@ -1,5 +0,0 @@
-/Users/daniel.beer/adventOfCode2023/AdventOfCode2023/hello_world/target/debug/deps/hello_world-dce19a1bcaf998ff: src/main.rs
-
-/Users/daniel.beer/adventOfCode2023/AdventOfCode2023/hello_world/target/debug/deps/hello_world-dce19a1bcaf998ff.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/daniel.beer/adventOfCode2023/AdventOfCode2023/hello_world/target/debug/hello_world: /Users/daniel.beer/adventOfCode2023/AdventOfCode2023/hello_world/src/main.rs
